0765542d92abdca643632dc316461e29c1a7bef3
junicotandiago198 <junicotandiagoo21@gmail.com>
2023-12-25 22:13:34 +0700

    API authentication, and product list

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user) {
+            return response([
+                'message'   => ['These credentials do not match our records.']
+            ], 400);
+        }
+
+        if(!Hash::check ($request->password, $user->password)) {
+            return response([
+                'messagge' => ['Password is wrong'],
+            ]. 404);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response([
+            'user'      => $user,
+            'token'     => $token
+        ], 200);
+    }
+
+    // logout
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logout success',
+        ]);
+    }
+}
